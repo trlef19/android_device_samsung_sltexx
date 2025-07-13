2496298ca9dec4500c86443f58e360610bb6130c
Lefteris T. <82327147+trlef19@users.noreply.github.com>
2025-07-13 13:04:37 +0300

    Update lineage_sltexx.mk

--- lineage_sltexx.mk
+++ lineage_sltexx.mk
@@ -1,5 +1,5 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/sltexx/device.mk)
@@ -16,8 +16,8 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=slte
 
 # Set screen resolution for bootanimation
-# vendor/cm/config/common_full_phone.mk
+# vendor/lineage/config/common_full_phone.mk
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
-# vendor/cm/config/common.mk
+# vendor/lineage/config/common.mk
 TARGET_BOOTANIMATION_HALF_RES := true
