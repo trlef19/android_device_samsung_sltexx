35039ba71c01ecc10a7a8151aaff2a9e63077054
Lefteris T. <82327147+trlef19@users.noreply.github.com>
2025-08-14 14:29:06 +0300

    new gpsd.cpp
    from exynos5433

--- shim/gpsd.cpp
+++ shim/gpsd.cpp
@@ -17,48 +17,17 @@
 #define LOG_TAG "libshim_gpsd"
 #define LOG_NDEBUG 0
 
-#include <stdlib.h>
-#include <stdint.h>
-
-#include <gui/SensorManager.h>
-#include <utils/Log.h>
-
-namespace android {
+#include <sys/types.h>
 
 extern "C" {
-    SensorManager *_ZN7android9SingletonINS_13SensorManagerEE9sInstanceE = NULL;
-    Mutex _ZN7android9SingletonINS_13SensorManagerEE5sLockE(Mutex::PRIVATE);
+  ssize_t _ZN7android13SensorManager13getSensorListEPPKPKNS_6SensorE(void* thiz, void* list);
 
+  ssize_t _ZNK7android13SensorManager13getSensorListEPPKPKNS_6SensorE(void* thiz, void* list) {
+    return _ZN7android13SensorManager13getSensorListEPPKPKNS_6SensorE(thiz, list);
+  }
+}
 
-    void *CRYPTO_malloc(uint32_t size, const char *file __unused, uint32_t line __unused)
-    {
-        return calloc(size, 1);
-    }
-
-    long SSL_CTX_ctrl(void *ctx __unused, int cmd, long larg __unused, void *parg __unused)
-    {
-        ALOGE("SSL_CTX_ctrl cmd: %d", cmd);
-
-        return 0;
-    }
-
-    void _ZN7android13SensorManagerC1ERKNS_8String16E(void* obj, const String16& opPackageName);
-    void _ZN7android13SensorManagerC1Ev(void* obj)
-    {
-        _ZN7android13SensorManagerC1ERKNS_8String16E(obj, String16());
-    }
-
-    void *_ZN7android13SensorManager16createEventQueueENS_7String8Ei(void* obj, String8 packageName, int mode);
-    void *_ZN7android13SensorManager16createEventQueueEv(void* obj)
-    {
-        return _ZN7android13SensorManager16createEventQueueENS_7String8Ei(obj, String8(""), 0);
-    }
-
-    ssize_t _ZN7android13SensorManager13getSensorListEPPKPKNS_6SensorE(void *obj, Sensor const* const** list);
-    ssize_t _ZNK7android13SensorManager13getSensorListEPPKPKNS_6SensorE(void *obj, Sensor const* const** list)
-    {
-        return _ZN7android13SensorManager13getSensorListEPPKPKNS_6SensorE(obj, list);
-    }
-} /* extern */
-
-} /* namespace */
+extern "C" int _ZN7android13SensorManagerD1Ev();
+extern "C" int _ZN7android13SensorManagerC1Ev() {
+    return _ZN7android13SensorManagerD1Ev();
+}
