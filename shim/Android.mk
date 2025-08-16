2b5bca29cc881a3a0bd93f78363fc0b908f690e4
Lefteris T. <82327147+trlef19@users.noreply.github.com>
2025-08-16 20:46:41 +0300

    add missing local shared lib

--- shim/Android.mk
+++ shim/Android.mk
@@ -28,7 +28,7 @@ include $(BUILD_SHARED_LIBRARY)
 ### GPSD
 include $(CLEAR_VARS)
 
-LOCAL_SHARED_LIBRARIES := libgui liblog libutils
+LOCAL_SHARED_LIBRARIES := libgui liblog libutils libsensor
 LOCAL_SRC_FILES := gpsd.cpp
 LOCAL_MODULE := libshim_gpsd
 LOCAL_MODULE_TAGS := optional
