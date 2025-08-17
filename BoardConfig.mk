8d14dc4d53dc9eede8f58da496e6f1de70219670
Lefteris T. <82327147+trlef19@users.noreply.github.com>
2025-08-17 18:14:15 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -312,7 +312,7 @@ TW_THEME := portrait_hdpi
 TARGET_RECOVERY_PIXEL_FORMAT := "BRGA_8888"
 TARGET_RECOVERY_DEVICE_MODULES += prebuilt_file_contexts init.recovery.usb.rc
 
-TARGET_OTA_ASSERT_DEVICE := sltexx,slte,slteskt
+TARGET_OTA_ASSERT_DEVICE := sltexx,slte,,slteskt
 
 # Add logcat support
 TWRP_INCLUDE_LOGCAT := true
